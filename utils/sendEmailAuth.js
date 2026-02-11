5807133ba95207252f4d056671744e8226d90e7d
sarvantrendofficial <sarvantrendofficial@gmail.com>
2026-02-11 23:06:49 +0530

    Update sendEmailAuth.js new one

--- utils/sendEmailAuth.js
+++ utils/sendEmailAuth.js
@@ -9,8 +9,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // Use true for port 465, false for all other ports
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: 'sarvantrendofficial@gmail.com',
+        pass: 'zeepwohfsbbgmezh'
     },
     tls: {
         rejectUnauthorized: false
